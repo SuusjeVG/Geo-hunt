a224fe56078b5bb9fab60ff393db3a37b2916945
Susan <suusjevanglabbeek@live.nl>
2025-01-21 22:31:06 +0100

    added more padding to the buttons

--- resources/views/components/game-navigation.blade.php
+++ resources/views/components/game-navigation.blade.php
@@ -3,7 +3,7 @@
     'name' => 'Button', 
 ])
 
-<button {{ $attributes->merge(['class' => 'flex items-center justify-center px-1 py-1 md:px-4 md:py-2 rounded']) }}>
+<button {{ $attributes->merge(['class' => 'flex items-center justify-center px-2 py-1 md:px-4 md:py-2 rounded']) }}>
     {{ $slot }}
     {{-- <img class="h-6 w-auto" x-bind:src="url" alt="{{ $name }}"> --}}
 </button>
